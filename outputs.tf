ca37835d6c6abf91302cd4bec7340eeb3c50b1b8
stream2stream <swright@celestial.co.uk>
2023-04-07 23:21:04 +0100

    Output EC2 DNS names

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "instance_arn" {
 output "ec2_global_ips" {
   value = ["${aws_instance.blog.*.public_ip}"]
 }
+
+output "ec2_global_domainNames" {
+  value = ["${aws_instance.blog.*.public_dns}"]
+}
