4db1b4505d62f30e632ea515261eaddbd5d70ecb
stream2stream <swright@celestial.co.uk>
2023-04-11 00:00:51 +0100

    Added autoscaling

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,3 @@
-output "instance_ami" {
-  value = aws_instance.blog.ami
-}
-
-output "instance_arn" {
-  value = aws_instance.blog.arn
-}
 
 output "ec2_global_ips" {
   value = ["${aws_instance.blog.*.public_ip}"]
