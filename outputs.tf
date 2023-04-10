09080114ded233108e6295052ebd555ea5978d34
stream2stream <swright@celestial.co.uk>
2023-04-11 00:15:14 +0100

    Fixed error in output variables

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 
 output "ec2_global_ips" {
-  value = ["${module.blog_alb.*.public_ip}"]
+  value = ["${module.blog_alb.public_ip}"]
 }
 
 output "ec2_global_domainNames" {
-  value = ["${module.blog_alb.*.public_dns}"]
+  value = ["${module.blog_alb.public_dns}"]
 }
