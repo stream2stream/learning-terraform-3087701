86f86f405b63271a1539e833dacc40822fb858c1
stream2stream <swright@celestial.co.uk>
2023-04-13 00:41:11 +0100

    Missing end quotes in variables file

--- variables.tf
+++ variables.tf
@@ -32,11 +32,11 @@ variable "environment" {
 }
 
 variable "asg_min_size" {
-  description = "Minimum number of instances in ASG
+  description = "Minimum number of instances in ASG"
   default = 1
 }
 
 variable "asg_max_size" {
-  description = "Maximum number of instances in ASG
+  description = "Maximum number of instances in ASG"
   default = 2
 }
